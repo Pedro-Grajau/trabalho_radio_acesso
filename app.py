f4997c5ebef391a461476f2b0963e7a79dd9d1c4
Pedro Lucas Grajaú <84031272+Pedro-Grajau@users.noreply.github.com>
2022-07-07 21:37:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from okumura_hata import calculate_path_loss, create_line_plot
 from map_folium import create_map
 
 ### STREAMLIT
+st.set_page_config(page_title='Rádio Acesso', page_icon=':satellite:')
 
 st.markdown("### :satellite: Predição de Perda de Potência")
 st.text('')
@@ -35,11 +36,16 @@ map_folium = create_map(ERB, DESTINY, dp)
 d = geodesic(ERB, DESTINY).km
 st_map = st_folium(map_folium, height = 450, width = 725)
 
+st.warning(f'Distância entre os pontos é de {round(d, 2)}km')
 if st.button('Calcular'):
-    path_loss = calculate_path_loss(f, Hb, Hm, d, types.index(t))
-    table = create_line_plot(f, Hm, Hb, dp)
+    if d > dp:
+        st.error("A distância entre os pontos está fora do range de acesso")
+    elif f > 2200 or f < 150:
+        st.error("A frequencia permitida está entre 150MHz e 2200MHz")
+    else:
+        path_loss = calculate_path_loss(f, Hb, Hm, d, types.index(t))
+        table = create_line_plot(f, Hm, Hb, dp)
     
-    st.info(f'A perda de potência localizada é de {round(path_loss, 2)} decibéis')
-
-    st.markdown("#### Gráfico de perda de potência pela distância")
-    st.line_chart(table)
+        st.info(f'A perda de potência localizada é de {round(path_loss, 2)} decibéis')
+        st.markdown("#### Gráfico de perda de potência pela distância")
+        st.line_chart(table)
