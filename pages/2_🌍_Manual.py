9b696adb35a117598e08430fc24a5d2fe99fd2fc
ThyagoKZKR <kzkr.thyago@gmail.com>
2022-07-11 19:23:22 -0300

    finalizado o manual em markdown e no app

--- pages/2_🌍_Manual.py
+++ pages/2_🌍_Manual.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from pathlib import Path
 
 st.markdown("#### Manual :books: ")
+
+section = "## Seleção da ERB e Receptor\n- Para definir a localização da ERB e do receptor, é necessário definí-las nos seguintes campos. Após digitar os campos, pressione enter"
+st.markdown(section, unsafe_allow_html=True)
+st.image('assets/origin_destiny.png')
+
+section = "## Definição das Variáveis\n- Na barra esquerda, é possível entrar com os valores do modelo que servem de entrada para o modelo"
+st.markdown(section, unsafe_allow_html=True)
+st.image('assets/side_bar.png')
